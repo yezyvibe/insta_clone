65707c02e8f1036ab1cd606a0407a2865597af10
yezyvibe <yeji0501@gmail.com>
2020-06-26 17:22:50 +0900

    Article model migrations

---
+++ back_end/articles/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "articles"
+
+urlpatterns = [
+    # path('', views.article_list),
+    # path('create/', views.create_atricle),
+    # path('<int:article_pk>', views.artiecle_detail),
+]
--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # DRF
+    'rest_framework',
+
     # my apps
     'accounts',
     'articles',
