c916583e95d08ae21c0c4cf80af842144ff84334
yezyvibe <yeji0501@gmail.com>
2020-07-11 10:50:12 +0900

    user_articles 불러오기

--- front_end/src/components/profiles/ArticleList.vue
+++ front_end/src/components/profiles/ArticleList.vue
@@ -1,24 +1,33 @@
 <template>
   <div>
     <h1>ArticleList</h1>
-    게시글 보여주는 공간
+    <span>
+      {{ articles }}
+    </span>
   </div>
 </template>
 
 <script>
-// import axios from 'axios'
-// const SERVER_URL = 'http://localhost:8000'
+import axios from 'axios'
+const SERVER_URL = 'http://localhost:8000'
+
 export default {
   name: 'ArticleList',
   data() {
     return {
+      username : this.$route.params.username,
       articles: []
     }
   },
   methods: {
     fetchArticles() {
-
+      axios.get(SERVER_URL + '/articles/' + this.username)
+        .then(res => this.articles = res.data)
+        .catch(err => console.error(err))
     }
+  },
+  created() {
+    this.fetchArticles()
   }
 }
 </script>
